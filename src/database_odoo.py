d1e9e96d76784c66f3e2ec5013536a051bfffc6e
anbinh-dbd <thetaichu@gmail.com>
2022-11-30 16:30:16 +0700

    taict commit 30112022 16h30

--- src/database_odoo.py
+++ src/database_odoo.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'postgresql://admin:abbankadmin@10.32.13.58:5432/odoo_db'
+SQLALCHAMY_DATABASE_URL = 'postgresql://admin:abbankadmin@10.32.13.58:5432/odoo_db_dev'
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
