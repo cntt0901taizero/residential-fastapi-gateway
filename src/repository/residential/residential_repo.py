aca1e8eda9b3026730c124a83f911705932fa485
anbinh-dbd <thetaichu@gmail.com>
2022-12-05 14:01:57 +0700

    taict get user join partner

--- src/repository/residential/residential_repo.py
+++ src/repository/residential/residential_repo.py
@@ -7,9 +7,13 @@ from src.schemas.residential import common_dto
 
 
 async def get_user_by_id(uid: int, db: Session):
-    sql = text("select id, active, login, company_id, partner_id, "
-               "create_date, signature, notification_type, phone_number "
-               "from res_users where id = " + str(uid))
+    sql = text("select res_users.id, res_users.active, res_partner.name, res_partner.display_name, "
+               "res_users.login, res_partner.lang, res_partner.tz, res_users.create_date, res_users.signature, "
+               "res_partner.phone, res_partner.email, "
+               "concat('" + get_settings().residential_server_url +
+               "', '/web/image?', 'model=res.users&id=', res_users.id , '&field=avatar_1920') as image_url "
+               "from res_users inner join res_partner on res_users.partner_id = res_partner.id "
+               "where res_users.id = " + str(uid))
     # result = (db.execute(sql)).fetchall()
     result = [dict(row) for row in db.execute(sql)]
     # result[0]['password'] = ''
