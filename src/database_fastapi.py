3883940fb472b218a56badc1e37e2f8c1d63d2e0
anbinh-dbd <thetaichu@gmail.com>
2022-12-05 13:29:51 +0700

    taict db connection close

--- src/database_fastapi.py
+++ src/database_fastapi.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+import logging
+
+logger = logging.getLogger(__name__)
 
 SQLALCHAMY_DATABASE_URL = 'postgresql://admin:abbankadmin@10.32.13.58:5432/fastapi_db_dev'
-engine = create_engine(SQLALCHAMY_DATABASE_URL)
+engine = create_engine(SQLALCHAMY_DATABASE_URL, poolclass=NullPool)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
 
@@ -12,6 +16,10 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        logger.exception('Session rollback cause EX: ' + str(e.args[0]))
+        db.rollback()
+        raise
     finally:
         db.close()
 
--- src/database_odoo.py
+++ src/database_odoo.py
@@ -1,10 +1,15 @@
+from psycopg2 import pool
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+import logging
 
-SQLALCHAMY_DATABASE_URL = 'postgresql://admin:abbankadmin@10.32.13.58:5432/odoo_db_dev'
-engine = create_engine(SQLALCHAMY_DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+logger = logging.getLogger(__name__)
+
+SQLALCHAMY_DATABASE_URL = 'postgresql+psycopg2://admin:abbankadmin@10.32.13.58:5432/odoo_db_dev'
+engine = create_engine(SQLALCHAMY_DATABASE_URL, poolclass=NullPool)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
 
@@ -12,7 +17,12 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        logger.exception('Session rollback cause EX: ' + str(e.args[0]))
+        db.rollback()
+        raise
     finally:
         db.close()
+
 
 
