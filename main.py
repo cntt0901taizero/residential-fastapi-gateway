a774c96a1ee22be1314c92c978f695223357b54b
anbinh-dbd <thetaichu@gmail.com>
2022-12-23 11:23:44 +0700

    taict logs file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.database_fastapi import engine
 from src.routers import user_fastapi, auth_fastapi
 from src.routers.residential import residential, userauth
 from config import get_settings
+import logging
 import uvicorn
 
 app = FastAPI()
@@ -16,7 +17,18 @@ app.include_router(userauth.router)
 app.include_router(residential.router)
 
 
+@app.on_event("startup")
+async def startup_event():
+    logger = logging.getLogger("uvicorn.access")
+    handler = logging.FileHandler('logs_file.log', mode='w')
+    # handler.setLevel(level=logging.INFO)
+    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s [ in %(pathname)s:%(lineno)d] %(message)s"))
+    logger.addHandler(handler)
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host=get_settings().uvicorn_host, port=get_settings().uvicorn_port)
+
+
 
 
