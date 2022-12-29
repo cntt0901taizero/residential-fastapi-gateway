7ec531cb05a6a362cf8c2a96e68b259b0f1d6172
anbinh-dbd <thetaichu@gmail.com>
2022-12-29 15:08:44 +0700

    taict env config commit

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
-    residential_server_url: str = "http://10.32.13.57:8069"
-    uvicorn_host: str = "10.32.47.62"
-    uvicorn_port: int = 8000
+    residential_server_url: str = "http://127.0.0.1:8069"
+    uvicorn_host: str = "10.32.13.57"
+    uvicorn_port: int = 8081
 
     class Config:
         env_file = ".env"
