2def56f8ba96af19ae4d6246f8a4d945db49a0a3
Binhpt <binhpt@abbank.vn>
2023-02-28 15:13:53 +0700

    update

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.repository import user_repo
 from app.schemas import FullInfoUser
+from app.utilities.common import get_data_image
 from configs import get_settings
 
 
@@ -29,7 +30,7 @@ async def get_detail_user(db: Session, user_id):
     try:
         user_detail = await user_repo.get_full_info(db, user_id)
         user_detail = FullInfoUser.from_orm(user_detail).dict()
-
+        user_detail['image'] = f"{get_settings().residential_server_url}/web/image?model=res.users&id={user_detail.get('id')}&field=avatar_1920"
         user_block_house = await user_repo.get_user_blockhouse(db, user_id)
         user_detail['block_house'] = user_block_house
         return user_detail
